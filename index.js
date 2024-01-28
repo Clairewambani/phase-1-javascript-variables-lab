500207b857dca522814e2f8788a4e27de983cc59
Your Name <you@example.com>
2024-01-28 12:12:44 +0300

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+// index.js
+
+let companyName = 'Scuber'; // First test
+
+const mostProfitableNeighborhood = 'Chelsea'; // Second test
+const companyCeo = 'Susan Smith'; // Third test
+
